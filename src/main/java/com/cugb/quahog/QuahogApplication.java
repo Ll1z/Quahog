6e4429e878d4eaedec9ca90b8b80be683d7a331e
Lilz <1272183319@qq.com>
2025-05-29 13:45:25 +0800

    构建基本框架

---
+++ src/main/java/com/cugb/quahog/QuahogApplication.java
@@ -0,0 +1,13 @@
+package com.cugb.quahog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuahogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuahogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cugb/quahog/QuahogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cugb.quahog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuahogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
